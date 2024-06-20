5135aa06406662a3a2b9dca6c5d25845d56d0257
Brittney Epperson <beppers87@hotmail.com>
2024-06-20 13:05:28 -0500

    Added logic to hide navBar until scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
 import './App.css';
 
 function App() {
 
+  const [showNav, setShowNav] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > window.innerHeight) {
+        setShowNav(true);
+      } else {
+        setShowNav(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+
+  }, []);
+
   return (
-    <>
-      <NavBar />
+    <div className='App'>
+      {showNav && <NavBar />}
       <Home />
-    </>
+    </div>
   )
 }
 
