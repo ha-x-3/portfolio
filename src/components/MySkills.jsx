94e21ab93d54906281d79f5dbb01c5f19eeb602d
Brittney Epperson <beppers87@hotmail.com>
2024-06-20 14:29:37 -0500

    Added mySkills component

--- src/components/MySkills.jsx
+++ src/components/MySkills.jsx
@@ -1,12 +1,48 @@
-import React from 'react'
+import React from 'react';
 
 export default function MySkills() {
-  return (
+	return (
 		<div className='mySkills'>
-			<h1>MY EXPERIENCE</h1>
-			<p>
-				&lt; A LIFE AND TECHNICAL <span>PERSPECTIVE</span> &#8725;&gt;
-			</p>
+			<div className='skillsContent'>
+				<div className='skillsHeading'>
+					<h1>MY EXPERIENCE</h1>
+					<p>
+						&lt; A LIFE AND TECHNICAL{' '}
+						<span className='bold'>PERSPECTIVE</span> &#8725;&gt;
+					</p>
+				</div>
+				<div className='skills'>
+					<div className='hardSkills'>
+						<h1>HARD SKILLS</h1>
+						<ul>
+							<li>HTML</li>
+							<li>CSS</li>
+							<li>JAVASCRIPT</li>
+							<li>JAVA</li>
+							<li>SQL</li>
+							<li>VSCODE</li>
+							<li>INTELLIJ</li>
+							<li>MY SQL WORKBENCH</li>
+							<li>REACT</li>
+							<li>GIT & GITHUB</li>
+							<li>LIGHT AUTOMATED TESTING</li>
+						</ul>
+					</div>
+					<div className='softSkills'>
+						<h1>SOFT SKILLS</h1>
+						<ul>
+							<li>QUICK AND CURIOUS LEARNER</li>
+							<li>ATTENTION TO DETAIL</li>
+							<li>EXCELLENT TIME MANAGEMENT</li>
+							<li>GREAT COLLABORATION AND TEAMWORK</li>
+							<li>RESILIENCE AND ADAPTABILITY</li>
+							<li>ABILITY TO FOLLOW DETAILED INSTRUCTION</li>
+							<li>GOOD JUDGEMENT SKILLS</li>
+							<li>PRECISE DELIVERY IN HIGH STRESS, FAST-PACED ENVIRONMENT</li>
+						</ul>
+					</div>
+				</div>
+			</div>
 		</div>
-  );
+	);
 }
