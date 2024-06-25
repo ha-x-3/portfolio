9f02145d303b860edfd97553fd0b9879af4fa8ad
Brittney Epperson <beppers87@hotmail.com>
2024-06-25 12:09:32 -0500

    Added contact links

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,9 +6,46 @@ export default function Contact() {
 			<div className='contactHeading'>
 				<h1>LET'S CONNECT</h1>
 				<p>
-					&lt; SIMPLY CHOOSE A{' '}
-					<span className='bold'>PLATFORM</span> &#8725;&gt;
+					&lt; SIMPLY CHOOSE A <span className='bold'>PLATFORM</span>{' '}
+					&#8725;&gt;
 				</p>
+			</div>
+			<div className='contactLinks'>
+				<a
+					href='https://github.com/ha-x-3'
+					target='_blank'
+					className='contactDetail'
+				>
+					<i className='fa-brands fa-github-square'></i> Github
+				</a>
+				<a
+					href='https://www.linkedin.com/in/brittney-epperson/'
+					target='_blank'
+					className='contactDetail'
+				>
+					<i className='fa-brands fa-linkedin'></i> Linkedin
+				</a>
+				<a
+					href='https://www.freecodecamp.org/Beppers87'
+					target='_blank'
+					className='contactDetail'
+				>
+					<i className='fa-brands fa-free-code-camp'></i> freeCodeCamp
+				</a>
+				<a
+					href='https://www.facebook.com/brittney.epperson'
+					target='_blank'
+					className='contactDetail'
+				>
+					<i className='fa-brands fa-facebook-square'></i> Facebook
+				</a>
+				<a
+					href='mailto:beppers87@hotmail.com'
+					target='_blank'
+					className='contactDetail'
+				>
+					<i className='fas fa-at'></i> Beppers87@hotmail.com
+				</a>
 			</div>
 		</div>
   );
