83b16327c1fcd7434c412b281382ca1aae334a0c
Brittney Epperson <beppers87@hotmail.com>
2024-06-27 17:12:35 -0500

    Adjusted navBar to hold formatting during change in viewport size

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,36 +6,38 @@ export default function NavBar({ showNav}) {
 	return (
 		<>
 			<div className={`navBar ${showNav ? 'show' : ''}`}>
-				<img
-					src={navLogo}
-					alt='Brittney Epperson <A phoenix who CODES/>'
-					id='navLogoImage'
-				/>
-				<div className='navLinks'>
-					<a
-						href='#home'
-						className='navLink'
-					>
-						HOME
-					</a>
-					<a
-						href='#skills'
-						className='navLink'
-					>
-						MY SKILLS
-					</a>
-					<a
-						href='#work'
-						className='navLink'
-					>
-						MY WORK
-					</a>
-					<a
-						href='#contact'
-						className='navLink'
-					>
-						CONTACT
-					</a>
+				<div className='navContent'>
+					<img
+						src={navLogo}
+						alt='Brittney Epperson <A phoenix who CODES/>'
+						id='navLogoImage'
+					/>
+					<div className='navLinks'>
+						<a
+							href='#home'
+							className='navLink'
+						>
+							HOME
+						</a>
+						<a
+							href='#skills'
+							className='navLink'
+						>
+							MY SKILLS
+						</a>
+						<a
+							href='#work'
+							className='navLink'
+						>
+							MY WORK
+						</a>
+						<a
+							href='#contact'
+							className='navLink'
+						>
+							CONTACT
+						</a>
+					</div>
 				</div>
 			</div>
 			<div className={`navGradient ${showNav ? 'show' : ''}`}></div>
