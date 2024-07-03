b2c4c0a421307f20f916505e6bfbde7f55379c33
Brittney Epperson <beppers87@hotmail.com>
2024-07-03 17:01:45 -0500

    Separated Home image into 2 images for animation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import logoReflection from '../assets/LogoReflection.svg';
+import logo from '../assets/BEP_LOGO_LARGE_TOP.svg';
+import logoReflection from '../assets/BEP_LOGO_LARGE_REFLECTION.svg';
 import '../App.css';
 
 export default function Home() {
   return (
     <div className='home' id='home'>
       <div className='homeImage'>
-        <img src={logoReflection} alt="Brittney Epperson <A phoenix who CODES/>" id='phoenixImage'/>
+        <img src={logo} alt="Brittney Epperson <A phoenix who CODES/>" id='phoenixImage'/>
+        <img src={logoReflection}  alt="</ SEDOC ohw xineohp A> nosreppE yenttirB" id='phoenixImageReflection' />
       </div>
     </div>
   )
